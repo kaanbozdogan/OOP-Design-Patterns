bf0d4407cb1dade489d6d63767096c9503043139
kaanbozdogan <32725324+kaanbozdogan@users.noreply.github.com>
2022-10-19 12:37:51 +0300

    update wrong files with right ones

---
+++ hw1/src/FireCharacter.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+/**
+ * A character with fire properties.
+ */
+public class FireCharacter extends AbstractCharacter {
+
+	public FireCharacter() {
+		super(100, 125, 75, Color.red, "Fire");
+	}
+}
---
+++ hw1/src/NatureCharacter.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+/**
+ * A character with nature properties.
+ */
+public class NatureCharacter extends AbstractCharacter {
+
+	public NatureCharacter() {
+		super(75, 100, 125, Color.green, "Nature");
+	}
+}
